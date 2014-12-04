1dcbb0a346ec24eec01cac0b073e33bd27b23c65
Alex Morega <alex@grep.ro>
2014-12-04 17:00:33 +0200

    port devel environment from the frontend repo

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename'),
     browserify = require('gulp-browserify'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    Q = require('q'),
+    http = require('http'),
+    express = require('express'),
+    proxy = require('simple-http-proxy');
+
+var KIELER_URL = 'http://layout.rtsys.informatik.uni-kiel.de:9444/live';
 
 
 gulp.task('build', function() {
@@ -23,6 +29,24 @@ gulp.task('build', function() {
 gulp.task('auto', function() {
   gulp.start('build');
   gulp.watch('src/**/*', ['build']);
+});
+
+
+gulp.task('devel', function() {
+  gulp.start('auto');
+
+  var app = express(),
+      host = '0.0.0.0',
+      port = +(process.env.PORT || 5000);
+
+  app.use('/kieler', proxy(KIELER_URL));
+  app.use(express.static(__dirname + '/dist'));
+
+  http.createServer(app).listen(port, host, function() {
+    console.log('devel server listening on ' + host + ':' + port);
+  })
+
+  return Q.defer().promise;
 });
 
 
